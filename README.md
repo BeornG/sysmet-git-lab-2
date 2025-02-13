06a44b6bdb46ca8b66c13effb2ecc3b67a6a4c98
Daniel Kert <danielkertdev@gmail.com>
2025-02-13 21:01:32 +0200

    Ah nu. oops

--- README.md
+++
@@ -1,2 +0,0 @@
-# sysmet-git-lab-2
-Used for fork/pull request part of the version management lab
--- danielkert-dev.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("Hello from Daniel :)");
-	return 0;
-}
